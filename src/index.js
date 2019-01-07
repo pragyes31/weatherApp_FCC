069294437e21a62ecb1ad70ea8f7048c55f8dc88
Rahul Nayak <pragyes31@users.noreply.github.com>
2019-01-07 10:57:50 +0530

    added getCurrentLocation function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import "./styles.css";
 
-function weatherApp() {}
+function weatherAppFn() {
+  const latitude = "";
+  const longitude = "";
+  const weatherApp = {
+    getCurrentLocation: () => {
+      let cordinates = {
+        latitude: "",
+        longitude: ""
+      };
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+          (cordinates.latitude = position.coords.latitude),
+            (cordinates.longitude = position.coords.longitude);
+        });
+      } else {
+        throw Error("geolocation api not found");
+      }
+      console.log(cordinates);
+      return cordinates;
+    },
+    showWeather: () => {
+      let currentLocation = weatherApp.getCurrentLocation();
+      console.log(currentLocation);
+    }
+  };
+  weatherApp.showWeather();
+}
+
+const weatherApp = weatherAppFn();
